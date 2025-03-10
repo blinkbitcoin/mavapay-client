7492a5d7f0b3fee1f4feb2f3ec27e488ab50adf3
Esaú Gómez <alfonsoesaugomez@gmail.com>
2025-03-10 15:00:18 -0600

    chore: remove test file

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import { mavapayClient } from "."
-;(() => {
-  mavapayClient.configureAuth({
-    network: "mainnet",
-    apiKey: "1a0e27aec9b87c5fc98948ce6e2304b31954ca3a468",
-  })
-
-  mavapayClient.getBanksByCountry("NG")
-})()
