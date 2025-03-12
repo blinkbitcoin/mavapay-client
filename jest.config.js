b999a29b63d975cba8b83000d0d6ba06315f6d74
Esaú Gómez <alfonsoesaugomez@gmail.com>
2025-03-12 14:59:01 -0600

    chore(test): configure Jest to resolve @/ path aliases

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest",
   },
   modulePaths: ["<rootDir>/__tests__"],
+  modulePaths: ["<rootDir>/src"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
   testPathIgnorePatterns: ["/node_modules/"],
   fakeTimers: {
     enableGlobally: true,
