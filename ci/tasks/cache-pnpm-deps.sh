72b29e841bcfd2b23c9d23b8a6e986ea67153f33
k9ert <kim@blinkbtc.com>
2025-03-17 17:29:02 +0100

    fix: pnpm compatible bundling of deps

--- ci/tasks/cache-pnpm-deps.sh
+++ ci/tasks/cache-pnpm-deps.sh
@@ -5,9 +5,20 @@ set -eu
 tar_out="$(pwd)/bundled-deps"
 
 pushd deps
+# Install dependencies
 pnpm install
+
+# Get git reference for versioning
 git log --pretty=format:'%h' -n 1 > gitref
 
-tar -zcvf "${tar_out}/bundled-deps-v$(cat ../deps-version/number)-$(cat gitref).tgz" . > /dev/null
+# Create the output filename
+output_file="${tar_out}/bundled-deps-v$(cat ../deps-version/number)-$(cat gitref).tgz"
+
+# Use --dereference to convert hard links to regular files
+tar --dereference -zcf "$output_file" \
+    --exclude='.git' \
+    --exclude='.github' \
+    --exclude='ci' \
+    . > /dev/null
 
 popd
