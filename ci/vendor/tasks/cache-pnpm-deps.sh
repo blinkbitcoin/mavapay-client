cd13f852f8a0f0cca9f25016cc3a5f3c92d72462
k9ert <kim@blinkbtc.com>
2025-03-19 17:38:57 +0100

    chore: more changes

--- ci/vendor/tasks/cache-pnpm-deps.sh
+++ ci/vendor/tasks/cache-pnpm-deps.sh
@@ -6,25 +6,23 @@ tar_out="$(pwd)/bundled-deps"
 
 pushd deps
 # Install dependencies
-echo "  * pnpm install"
+echo "    --> pnpm install"
 pnpm install --shamefully-hoist
 
 # Get git reference for versioning
-echo "  * git log"
+echo "    --> git log"
 git log --pretty=format:'%h' -n 1 > gitref
 
 # Create the output filename
 output_file="${tar_out}/bundled-deps-v$(cat ../deps-version/number)-$(cat gitref).tgz"
 
 # Use --dereference to convert hard links to regular files
-echo "  * tar ..."
+echo "    --> tar ..."
 tar --dereference -zcf "$output_file" \
     --exclude='.git' \
     --exclude='.github' \
     --exclude='ci' \
     .
 
-echo "  * Contents:"
-tar -ztvf "$output_file"
-echo "  * Done!"
+echo "    --> Done!"
 popd
