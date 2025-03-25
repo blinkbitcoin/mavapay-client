c2ace3c03ccbf726c6bee532839e9f6a80f8203f
k9ert <kim@blinkbtc.com>
2025-03-25 15:59:24 +0100

    fix: add --dereference again

--- ci/vendor/tasks/nodejs-cache-pnpm-deps.sh
+++ ci/vendor/tasks/nodejs-cache-pnpm-deps.sh
@@ -17,7 +17,7 @@ git log --pretty=format:'%h' -n 1 > gitref
 output_file="${tar_out}/bundled-deps-v$(cat ../deps-version/number)-$(cat gitref).tgz"
 
 echo "    --> tar ..."
-tar -zcf "$output_file" \
+tar --dereference -zcf "$output_file" \
     --exclude='.git' \
     --exclude='.github' \
     --exclude='ci' \
